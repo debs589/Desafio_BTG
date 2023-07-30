9240c327499b04803ec60b277b639194bed96962
debs589 <debora.carvalho589@gmail.com>
2023-07-30 18:08:02 -0300

    Cadastro de transações

--- src/main/java/com/example/desafioBTG/controllers/TransacaoController.java
+++ src/main/java/com/example/desafioBTG/controllers/TransacaoController.java
@@ -1,11 +1,15 @@
 package com.example.desafioBTG.controllers;
 
+import com.example.desafioBTG.models.Conta;
 import com.example.desafioBTG.models.Transacao;
+import com.example.desafioBTG.services.ContaService;
 import com.example.desafioBTG.services.TransacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TransacaoController {
@@ -13,16 +17,21 @@ public class TransacaoController {
     @Autowired
     private TransacaoService transacaoService;
 
+    @Autowired
+    private ContaService contaService;
+
     @GetMapping("/transacao")
     public List<Transacao> listarTransacaos(){
         return transacaoService.listarTransacoes();
     }
     @PostMapping("/transacao")
     public Transacao criarTransacao(@RequestBody Transacao transacao){
-        return transacaoService.criarTransacao(transacao);
+        Transacao novaTransacao = transacaoService.criarTransacao(transacao);
+        Conta conta = contaService.listarContaId(transacao.getConta().getNumeroConta()).get();
+        BigDecimal saldoFinal =  conta.getSaldo().add(novaTransacao.getValor());
+        conta.setSaldo(saldoFinal);
+        contaService.criarConta(conta);
+        return novaTransacao;
     }
-    @PutMapping("/transacao")
-    public Transacao atualizarTransacao(@RequestBody Transacao transacao) {
-        return transacaoService.criarTransacao(transacao);
-    }
+
 }
