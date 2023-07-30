23d96e400d9dc877904266b01304af7978be21f9
debs589 <debora.carvalho589@gmail.com>
2023-07-30 14:35:14 -0300

    Setup do Postgresql

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into cliente (cpf, nome_completo, telefone, email, endereco) values('123', 'jose','23','@','rua');
-insert into conta  (numero_conta, cpf, tipo_conta, saldo) values(2, '123','poupanca',50);
-insert into transacao (id, numero_conta, valor) values(3, 2,20);
